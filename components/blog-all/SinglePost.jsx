cc7b4e08aa797f41e7d870ad5ce6e6dfa271bb78
ArtPleb2868 <plebanczyk.artur@gmail.com>
2025-09-16 14:47:31 +0200

    Podmiana title z bloga z h4 na h3

--- components/blog-all/SinglePost.jsx
+++ components/blog-all/SinglePost.jsx
@@ -9,7 +9,7 @@ const SinglePost = ({ category, title, intro, image, slug, index }) => {
             <div className='h-[438px] w-[468px] flex flex-col p-[32px] relative justify-between rounded-[5px] overflow-hidden group max-2xl:w-[400px] max-2xl:h-[400px] max-xl:w-[550px] max-xl:h-[320px] max-lg:w-[330px] max-lg:h-[330px] max-fold:w-[290px]'>
                 <Category name={category} />
                 <div className='z-[4]'>
-                    <h4 className='text-[25px] font-semibold line-clamp-3 mb-[8px] max-2xl:text-[20px] max-xl:line-clamp-2' dangerouslySetInnerHTML={{__html: title}}></h4>
+                    <h3 className='text-[25px] font-semibold line-clamp-3 mb-[8px] max-2xl:text-[20px] max-xl:line-clamp-2' dangerouslySetInnerHTML={{__html: title}}></h3>
                     <p className='text-[16px] text-[#B4B4B4] line-clamp-3 max-2xl:text-[14px]' dangerouslySetInnerHTML={{__html: intro}}></p>
                 </div>
 

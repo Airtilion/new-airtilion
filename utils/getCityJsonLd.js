1b617d3ac7507f270295fc27391cfc701c53b72f
JcobV01 <jakubwadycki@gmail.com>
2025-10-27 10:42:32 +0100

    nowy business schema

--- utils/getCityJsonLd.js
+++ utils/getCityJsonLd.js
@@ -1,20 +1,37 @@
-const getCityJsonLd = (cityName, slug) => {
-    return {
+const getCityJsonLd = (cityData, slug) => {
+    const city = cityData.city;
+    const pageUrl = `https://airtilion.com/${slug}`;
+    const serviceName = cityData.metaTitle || `Strony internetowe ${city}`;
+    
+    const serviceSchema = {
         "@context": "https://schema.org",
-        "@type": "LocalBusiness",
-        "name": "Airtilion",
-        "url": `https://airtilion.com/${slug}`,
-        "address": {
-            "@type": "PostalAddress",
-            "addressLocality": "Tarnów",
-            "addressCountry": "PL"
-        },
-        "areaServed": {
-            "@type": "Place",
-            "name": cityName
-        },
-        "description": `Strony internetowe ${cityName} – nowoczesne i responsywne witryny dla firm.`
-    }
-}
+        "@type": "Service",
+        
+        "serviceType": "Web development",
+        "name": serviceName,
+        "description": cityData.metaDescription, 
+        "url": pageUrl,
+                
+        "provider": {
+            "@type": "Organization",
+            "name": "Airtilion",
+            "url": "https://airtilion.com/",
+            "logo": "https://airtilion.com/airtilion-logo.svg",
 
-export default getCityJsonLd
+            "contactPoint": { 
+                "@type": "ContactPoint",
+                "telephone": "+48-720-177-174",
+                "contactType": "customer service"
+            }
+        },
+        
+        "areaServed": {
+            "@type": "City",
+            "name": city
+        }
+    };
+
+    return serviceSchema;
+};
+
+export default getCityJsonLd;
